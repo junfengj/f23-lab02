ddd306b9eff30644e3544b87a0d7e376b2b2f633
junfengj <junfengj@andrew.cmu.edu>
2023-09-05 20:48:33 -0400

    feat: Java- add some test case

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,14 +1,24 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
+
 
 public class Main {
     public static void main(String[] args) {
         Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(2.0);
+        Shape square = new Square(3.0);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer_rectangle = new Renderer(rectangle);
+        Renderer renderer_circle = new Renderer(circle);
+        Renderer renderer_square = new Renderer(square);
 
-        renderer.draw();
+
+        renderer_rectangle.draw();
+        renderer_circle.draw();
+        renderer_square.draw();
     }
 }
